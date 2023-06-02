ed9bd489a652444b054661b005f917147c528976
= <=>
2023-06-02 22:56:51 +0900

    複数人プレイの機能を実装しました

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-let space = 1;
 let imgId = String;
+let playerSpaces = [1,1,1,1];
+let count = 0;
+let player = 0;
+
+
 function rollDie() {
     die = Math.floor((Math.random() * 6)) + 1;
     return (die);
 }
 function roll() {
-    const target = document.getElementById("result");
-    const spaceElement = document.getElementById("space");
     let result = rollDie();
-    space += result;
+    player = (player + 1) % 4;
+    playerSpaces[player] += result;
+    let playerSpace = playerSpaces[player];
+
+    console.log(player);
+    console.log(playerSpaces);
+    console.log(playerSpace);
+    const target = document.getElementById("result");
+    const spaceElement = document.getElementById("space" + String(player));
+    
     imgId = `<img class='diceImg' src='img/dice01_a_0${String(result)}.png' style='width: 48px;'>`
     target.innerHTML = imgId;
-    if (space >= 50) {
-        spaceElement.innerHTML = "GOAL!<br>Roll a die to restart."
-        space = 1;
+
+    if (playerSpace >= 50) {
+        spaceElement.innerHTML = "GOAL!"
+        playerSpaces[player] = 1;
     } else {
-        spaceElement.innerHTML = "You are in " + space + indicator(space) + " space";
+        spaceElement.innerHTML = "You are in " + playerSpace + indicator(playerSpace) + " space";
     }
 }
 function indicator(i) {
